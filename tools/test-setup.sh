16afcc0be87ff0754214ca9022bf883f2cd3205e
Paul Belanger <pabelanger@redhat.com>
2018-03-06 17:17:03 -0500

    Install zoo.cfg for zookeeper testing
    Again, this is why we don't want to depend on zookeeper for testing,
    more distro specific things we need to carry.
    
    Change-Id: Id221cd955e789d4a11721777f32d21c227afecf3
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -4,5 +4,10 @@
 # it sets up the test system as needed.
 # Developers should setup their test systems in a similar way.
 
+# On fedora we need to copy sample config
+if [ -f /etc/zookeeper/zoo_sample.cfg ]; then
+    sudo cp /etc/zookeeper/zoo_sample.cfg /etc/zookeeper/zoo.cfg
+fi
+
 # Be sure zookeeper is started.
 sudo service zookeeper start
