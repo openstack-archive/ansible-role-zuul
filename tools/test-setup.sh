a246151d0b85b0f12d4a5cabc79255421a3d6218
Paul Belanger <pabelanger@redhat.com>
2018-03-06 15:38:48 -0500

    Add zookeeper to bindep
    This is to work around a bug in zuul, where it needs zookeeper to be
    installed to start properly.
    
    Change-Id: I75498a71af1290db1b2d1fb0214b5ecc393b1e4d
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

---
+++ tools/test-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -xe
+
+# This script will be run by OpenStack CI before unit tests are run,
+# it sets up the test system as needed.
+# Developers should setup their test systems in a similar way.
+
+# Be sure zookeeper is started.
+sudo service zookeeper start
