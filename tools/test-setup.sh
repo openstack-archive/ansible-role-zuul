e46ccb18f22990d5cdcc3756090230701dcd1b1f
Paul Belanger <pabelanger@redhat.com>
2018-07-27 13:38:15 -0400

    DNM: Test yarn
    Change-Id: I7a751e5ad23cb3db970cef19c24333fa1da916ae
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -3,6 +3,7 @@
 # This script will be run by OpenStack CI before unit tests are run,
 # it sets up the test system as needed.
 # Developers should setup their test systems in a similar way.
+LSBDISTCODENAME=$(lsb_release -cs)
 
 # On fedora we need to copy sample config
 if [ -f /etc/zookeeper/zoo_sample.cfg ]; then
@@ -11,3 +12,19 @@ fi
 
 # Be sure zookeeper is started.
 sudo service zookeeper start
+
+if type apt-get; then
+    # Install https transport - otherwise apt-get HANGS on https urls
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https
+    # Install recent NodeJS repo
+    curl -sS https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+    echo "deb https://deb.nodesource.com/node_8.x $LSBDISTCODENAME main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+    # Install yarn repo
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt-get update
+    sudo DEBIAN_FRONTEND=noninteractive \
+        apt-get -q --option "Dpkg::Options::=--force-confold" --assume-yes \
+        install nodejs yarn
+fi
